3e46d80d8a60c4591696b11d7d60160ca08b22b5
Md Masum Billah <masumbillah6584@gmail.com>
2023-02-26 01:14:56 +0600

    added selectable three dot btn

--- src/pages/components/Favourite/Favourite.js
+++ src/pages/components/Favourite/Favourite.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BsGrid3X3Gap } from "react-icons/bs";
-import { VscListFlat } from "react-icons/vsc";
 import { Link, useLocation } from 'react-router-dom';
 import GridView from './GridView';
 import ListView from './ListView';
@@ -34,15 +33,34 @@ const Favourite = () => {
                   />
                 </Link>
                 <Link to="/dashboard/drive/list-view">
-                  <VscListFlat
-                  
+                  {/* <VscListFlat
                     color={
                       pathname === "/dashboard/drive/list-view"
                         ? "blue"
                         : "gray"
                     }
                     className="w-[22px] h-[24px]"
-                  />
+                  /> */}
+                  <svg
+                    color={
+                      pathname === "/dashboard/drive/list-view"
+                        ? "blue"
+                        : "gray"
+                    }
+                    width="22"
+                    height="22"
+                    viewBox="0 0 25 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1.375 12H23.375M1.375 17.5H23.375M1.375 23H23.375M3.875 1H20.875C21.538 1 22.1739 1.28973 22.6428 1.80546C23.1116 2.32118 23.375 3.02065 23.375 3.75C23.375 4.47935 23.1116 5.17882 22.6428 5.69454C22.1739 6.21027 21.538 6.5 20.875 6.5H3.875C3.21196 6.5 2.57607 6.21027 2.10723 5.69454C1.63839 5.17882 1.375 4.47935 1.375 3.75C1.375 3.02065 1.63839 2.32118 2.10723 1.80546C2.57607 1.28973 3.21196 1 3.875 1V1Z"
+                      stroke="#324FF6"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
                 </Link>
               </div>
             </div>
--- src/pages/components/Favourite/GridView.js
+++ src/pages/components/Favourite/GridView.js
@@ -1,228 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiFillStar } from 'react-icons/ai';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const GridView = () => {
+  const [settingStatus, setSettingStatus] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+  const [imgId, setImgId] = useState(0);
+  const items = [...Array(24)];
     return (
       <div>
         <div className="grid grid-cols-6 justify-between gap-2 px-7 py-8 bg-white rounded">
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          <div className="relative border rounded-lg bg-slate-700 h-36 w-full">
-            <BsThreeDotsVertical className="absolute top-2 left-2" />
-            <AiFillStar color="orange" className="absolute top-2 right-2" />
-            <img
-            className='w-full h-full rounded-md'
-              src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="not found"
-            />
-          </div>
-          
+          {items.map((item, index) => (
+            <div key={index} className="relative border rounded-lg bg-slate-700 h-36 w-full">
+              <button className="absolute top-2 left-2">
+                <BsThreeDotsVertical
+                  onClick={() => {
+                    setSettingStatus(true);
+                    setFavorite(false);
+                    setImgId(index)
+                  }}
+                />
+              </button>
+              <button className="absolute top-2 right-2">
+                <AiFillStar
+                  onClick={() => {setFavorite(true); setImgId(index); setSettingStatus(false)}}
+                  color="orange"
+                />
+              </button>
+              <div
+                style={{
+                  border: " .1px solid rgba(0, 0, 0, 0.06)",
+                  boxShadow: "0px 24px 48px rgba(0, 0, 0, 0.05)",
+                }}
+                className={`${
+                  settingStatus && imgId=== index ? `block z-30 text-sm` : `hidden`
+                } absolute top-3 left-6 bg-white rounded-md`}
+              >
+                <li
+                  className="list-none py-[8px] rounded-t cursor-pointer px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {
+                    setSettingStatus(!settingStatus);
+                  }}
+                >
+                  View Details
+                </li>
+                <li
+                  className="list-none py-[8px] cursor-pointer px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {}}
+                >
+                  Open Gallary
+                </li>
+
+                <li
+                  className="list-none py-[8px] cursor-pointer px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {}}
+                >
+                  Download
+                </li>
+                <li
+                  className="list-none py-[8px] cursor-pointer px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {}}
+                >
+                  Remove from Favorite
+                </li>
+                <li
+                  className="list-none py-[8px] cursor-pointer rounded-b px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {}}
+                >
+                  Delete
+                </li>
+              </div>
+              <div
+                style={{
+                  border: " .1px solid rgba(0, 0, 0, 0.06)",
+                  boxShadow: "0px 24px 48px rgba(0, 0, 0, 0.05)",
+                }}
+                className={`${
+                  favorite && imgId===index ? `block z-30 text-sm` : `hidden`
+                } absolute top-7 right-0 bg-white rounded-md`}
+              >
+                <li
+                  className="list-none py-[8px] rounded-md cursor-pointer px-6 hover:bg-[#ddd] transition duration-300"
+                  onClick={() => {
+                    setFavorite(!favorite);
+                    setSettingStatus(false);
+                  }}
+                >
+                  Click To Unfavorite
+                </li>
+              </div>
+              <img
+              onClick={()=> setSettingStatus(false)}
+                className="w-full h-full rounded-md"
+                src="https://images.unsplash.com/photo-1676476752258-6ce9df0a4820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+                alt="not found"
+              />
+            </div>
+          ))}
         </div>
       </div>
     );
